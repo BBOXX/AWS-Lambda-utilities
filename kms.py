2af990e792caf2c515f44b43167954c1d191ffec
Hugggsy <hugo.depretz@gmail.com>
2018-08-02 10:28:41 +0100

    kms example

---
+++ kms.py
@@ -0,0 +1,17 @@
+
+import boto3
+from os import environ
+from base64 import b64decode
+
+# if used on an aws service which role has permissions to use kms no keys are needed, otherwise you will need to provide an appropriate user keys to the client constructor
+client = boto3.client('kms')
+
+
+def getPasswordsFromKMS(self, client, KMSKey):
+    """
+    Takes a boto3 KMS client and returns the decrypted string of the stored value
+    """
+    plainBytes = client.decrypt(
+        CiphertextBlob=b64decode(environ[KMSKey]))['Plaintext']
+    plainStringValue = plainBytes.decode()
+    return plainStringValue
